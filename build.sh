4f2668430dfbde1005b0554a2e3fce32ef890fae
Georg Sauthoff <mail@georg.so>
2018-01-22 23:19:45 +0100

    fix patchelf location, improve curl call

--- build.sh
+++ build.sh
@@ -8,7 +8,13 @@ outdir=$1
 specdir=$2
 specfile=$(ls *.spec)
 
-curl -L -O $url
+if [ ${url#https} = $url ]; then
+  just_https=""
+else
+  just_https="--proto-redir =https"
+fi
+curl $just_https --silent --show-error --fail -L -O $url
+sha256sum "$filename"
 echo "$checksum  $filename" | sha256sum -c -
 
 rpmbuild \
--- patchelf/build.sh
+++ patchelf/build.sh
@@ -7,7 +7,7 @@ env
 name=patchelf
 version=0.9
 filename=$name-$version.tar.bz2
-url=http://releases.nixos.org/$name/$filename
+url=http://releases.nixos.org/$name/$name-$version/$filename
 checksum=a0f65c1ba148890e9f2f7823f4bedf7ecad5417772f64f994004f59a39014f83
 
 . ../build.sh
