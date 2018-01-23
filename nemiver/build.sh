c4d22901a046ef8716dbb499899531da5367be12
Georg Sauthoff <mail@georg.so>
2018-01-23 10:44:36 +0100

    update checksum

--- nemiver/build.sh
+++ nemiver/build.sh
@@ -10,6 +10,10 @@ patch_version=6
 version=${short_version}.${patch_version}
 filename=$name-$version.tar.xz
 url=http://ftp.gnome.org/pub/GNOME/sources/$name/$short_version/$filename
-checksum=331ae34f2d18166199a7012dc777fbc5899e01f3e28909502957fcb6bef6963f
+# checksum of the tar.xz included in the SRPM
+#checksum=331ae34f2d18166199a7012dc777fbc5899e01f3e28909502957fcb6bef6963f
+# great, the checksum doesn't match current available downloads ...
+# ... a diff of both un-tarred versions doesn't show any differences, though
+checksum=85ab8cf6c4f83262f441cb0952a6147d075c3c53d0687389a3555e946b694ef2
 
 . ../build.sh
